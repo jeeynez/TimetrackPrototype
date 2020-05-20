39668d5273b73ea6f7958dbec0910e657b168638
Carlos Vasquez <carlosvasquez@hablacode.org>
2020-05-20 00:16:06 -0500

    adding time tracker functionality

--- src/Components/TimeTracker.js
+++ src/Components/TimeTracker.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 function TimeTracker () {
+    let hour;
+    let minute;
+    let second;
+    let startTime;
+    
+    function getCurrentTime() {
+      let currentTime = new Date();
+      startTime = Date.now();
+      hour = currentTime.getHours();
+      minute = currentTime.getMinutes();
+      second = currentTime.getSeconds();
+      let time = document.getElementById('time');
+      time.innerText = hour + ":" + minute + ":" + second
+    }
+    
+    function calculateTime() {
+      let endTime = Date.now();
+      let totalMilliSeconds = endTime - startTime;
+      let totalHours = totalMilliSeconds / 1000 / 60 / 60;
+      console.log(totalHours)
+      let result = document.getElementById('totalWorkTime')
+      result.innerText = "Total Time Worked " + totalHours
+    }
+
     return (
      <div>
         <h1 className="center-text">Time Tracker</h1>
-        <button>Clock In</button>
+        <button onClick={getCurrentTime}>Clock In</button>
         <h1 id='time'></h1>
-        <button>Clock Out</button>
+        <button onClick={calculateTime}>Clock Out</button>
         <h1 id='totalWorkTime'></h1>
      </div>
     )
